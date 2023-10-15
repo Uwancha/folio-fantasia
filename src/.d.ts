e5e3dcb6d6b19ad7788956defd0ced4de42fcb7e
Uwancha <sito2alemu@gmail.com>
2023-10-15 15:25:53 +0300

    Rename declare file

---
+++ src/.d.ts
@@ -0,0 +1,9 @@
+declare module 'vite-plugin-sass';
+
+declare module 'react-router-dom' {
+    export const Routes: any;
+    export const BrowserRouter: any;
+    export const Route: any;
+    export const Link: any
+}
+  
