61ffbf3e59bcedfbf0006fd667c30734b6511d19
Uwancha <sito2alemu@gmail.com>
2023-10-19 10:33:04 +0300

    refactor: Use icons for theme changing option instead of texts

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { ThemeContext } from "../App";
 import "../styles/nav.scss"
 
+import darkMode from "../assets/206.jpg";
+import lightMode from "../assets/26915.jpg"
+
 const Nav: React.FC = () => {
     const {theme, toggleTheme} = useContext(ThemeContext)
 
@@ -13,8 +16,8 @@ const Nav: React.FC = () => {
                 <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/about">About</Link>
-                <div onClick={toggleTheme}>
-                    {theme === "dark" ? (<span>light</span>) :  (<span>dark</span>) }
+                <div onClick={toggleTheme} className="theme">
+                    {theme === "dark" ? (<img src={darkMode} alt="dark mode icon" className="themeIconDark" />) :  (<img src={lightMode} alt="light mode icon" className="themeIconLight"/>) }
                 </div>
             </nav>
         </div>
