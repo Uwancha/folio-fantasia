ba5265272a5644ba5b119f4f75b8d5e0500ee4de
Uwancha <sito2alemu@gmail.com>
2023-10-19 11:48:16 +0300

    Improve the UI's of the Projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -22,15 +22,15 @@ interface  Project {
 const projects: Project[] = [
     {
       id: 1,
-      title: 'A shopping cart, an e-commerce site',
+      title: 'Shopping Cart',
       image: shoppingSiteImage,
-      summary: 'A full-featured e-commerce application built using React, Context API and other modern technologies. Users can browse products, view details, add to cart and checkout.',
+      summary: 'An e-commerce application built using React, Context API and other modern technologies. Users can browse available products, view details of each, add to cart and checkout. Users can also adjust the number of products they want to buy.',
       liveLink: 'https://uwancha-ecommerce-site.netlify.app',
       githubLink: 'https://github.com/Uwancha/shopping-cart'
     },
     {
       id: 2,
-      title: 'Memory card game',
+      title: 'Memory Card Game',
       image: memoryCardImage,
       summary: `This project was built while learning full stack web development through The Odin Project's curriculum.`,
       liveLink: 'https://cardgame-uwancha.netlify.app',
@@ -38,7 +38,7 @@ const projects: Project[] = [
     },
     {
       id: 3,
-      title: 'Weather app',
+      title: 'Weather App',
       image: weatherAppImage,
       summary: 'This is a weather application built using HTML, CSS, JavaScript, and webpack to bundle the files. The app fetches weather data from the WeatherAPI to provide real-time weather information for different locations.',
       liveLink: 'https://uwancha.github.io/weather-app',
@@ -46,7 +46,7 @@ const projects: Project[] = [
     },
     {
         id: 4,
-        title: 'fentale restaurant',
+        title: 'Fentale Restaurant',
         image: fentaleRestaurantImage,
         summary: 'This is a simple restaurant page built with HTML, CSS, and JavaScript, and bundled using webpack. The page is rendered dynamically by using JavaScript to manipulate DOM elements.',
         liveLink: 'https://uwancha.github.io/fentale-restaurant',
@@ -54,7 +54,7 @@ const projects: Project[] = [
       },
       {
         id: 5,
-        title: 'Cv maker',
+        title: 'Cv Maker',
         image: cvMakerSiteImage,
         summary: 'This project is a small CV application built using React. It allows users to enter their general information, educational experience, and practical experience. The application provides an intuitive interface for adding and editing the CV details, and it supports submitting the form and displaying the entered information.',
         liveLink: 'https://main--uwancha-cvmaker.netlify.app/',
@@ -68,11 +68,12 @@ const Projects: React.FC = () => {
     return (
         
             <article className={`article${theme}`}>
-                <h3>Some of my works</h3>
+                <h3>My <span className={`span${theme}`}>Projects</span></h3>
                 {projects.map((project) => (
                     <section key={project.id} className={`section${theme}`}>
                             <img src={project.image} alt={project.title} />
                             <div className={`div${theme}`}>
+                                <h3>{project.title}</h3>
                                 <p>{project.summary}</p>
                                 <div className={`links${theme}`}>
                                     <a href={project.liveLink}>Live preview</a>
