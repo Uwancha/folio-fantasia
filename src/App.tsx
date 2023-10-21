665b65e0959ffcd59462b9177060a71ab2bda6fa
Uwancha <sito2alemu@gmail.com>
2023-10-21 21:51:50 +0300

    Rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
-import './App.scss';
+import './App.css';
 
 import Home from './pages/Home';
 import About from './pages/About';
